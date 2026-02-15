7ffa9184cf797e58da194f4ab0f11870c4d2c6b8
slivchansky666 <alexei.stoianov3@gmail.com>
2026-02-15 12:56:41 +0200

    Update test_main.py

--- lesson20/test_main.py
+++ lesson20/test_main.py
@@ -3,7 +3,8 @@ def test_add_positive_num():
     assert  add(2, 3) == 5
 
 def test_add_neg_num():
-    assert add(-1,4) == -5
+    assert add(-1, 4) == 3
 
 def test_add_mixed_num():
+
     assert add(-2, 5) == 3
